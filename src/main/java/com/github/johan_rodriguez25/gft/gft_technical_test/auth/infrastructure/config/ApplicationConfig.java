2a8e24fd129071d51cdc9bba7738052f4db2441d
Johan Rodriguez <88631504+Johan-Rodriguez25@users.noreply.github.com>
2025-07-14 01:52:57 -0500

    Removed test and fix methods deprecated

--- src/main/java/com/github/johan_rodriguez25/gft/gft_technical_test/auth/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/github/johan_rodriguez25/gft/gft_technical_test/auth/infrastructure/config/ApplicationConfig.java
@@ -34,9 +34,10 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+
         authProvider.setPasswordEncoder(passwordEncoder());
+
         return authProvider;
     }
 
